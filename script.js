f23e3fde613b825ea00500c4b3ce6e2ee9ec2211
Ulan <creepsonhat@gmail.com>
2018-11-17 22:07:59 +0700

    JS fix

--- script.js
+++ script.js
@@ -1,43 +1,19 @@
-var minValue;
-var maxValue;
-var minValueInput = document.getElementById("minValue");
-var maxValueInput = document.getElementById("maxValue");
-updateMin();
-updateMax();
-minValueInput.addEventListener("keyup",updateMin);
-maxValueInput.addEventListener("keyup",updateMax);
-
-var generateButton = document.getElementById("generate");
-generateButton.addEventListener("click", generate);
-var generatedNumber = document.getElementById("generatedNumber");;
-
-
-var amountOfGeneration;
-
+var minValue = document.getElementById("minValue");
+var maxValue = document.getElementById("maxValue");
+var generatedNumber = document.getElementById("generatedNumber");
 var slider = document.getElementById("slider");
-var sliderValue = document.getElementById("sliderValue");
+var count = document.getElementById("count");
 
-slider.addEventListener("mouseup", updateAmount);
-
-function updateAmount(){
-	amountOfGeneration = slider.value;
-	sliderValue.innerHTML = amountOfGeneration + " чисел";
-	//alert(amountOfGeneration);
+function countUpdate(){
+	count.innerHTML = slider.value + " чисел";
 }
 
-function updateMin(){
-	minValue = minValueInput.value;
-}
-function updateMax(){
-	maxValue = maxValueInput.value;
-}
+function Generate(){
+	generatedNumber.innerHTML = null;
 
-function generate(){
-	var outputString="";
-	for (var i=0; i<amountOfGeneration; i++){
-		var gen = parseInt(Math.random() * (maxValue-minValue+1));
-		gen+=parseInt(minValue);
-		outputString=outputString + " " + gen;
+	for (var i=0; i<slider.value; i++){
+		generatedNumber.innerHTML += Math.floor(Math.random()*(parseInt(maxValue.value)-parseInt(minValue.value)+1))+parseInt(minValue.value) + " ";
 	}
-	generatedNumber.innerHTML=outputString;
 }
+
+slider.addEventListener("input", countUpdate);
